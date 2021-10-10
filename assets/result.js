6662c1892a12a0c2edca0a544a25dc548c99eebb
Olsen <chrisolsen1993@gmail.com>
2021-10-10 19:53:56 -0400

    work on some staff

--- assets/result.js
+++ assets/result.js
@@ -1,16 +1,18 @@
 function getParams() {
     // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-    var searchParamsArr = document.location.search.split('&');
-  
+    var parameterSearch = document.location.search.split('&');
+  console.log(parameterSearch)
     // Get the query and format values
-    var queryJob = searchParamsArr[0].split('=').pop();
-    var queryLevel = searchParamsArr[1].split('=').pop();
-    var queryIndustry =searchParamsArr[2].split('=').pop();
-    searchApi(queryJob, queryLevel, queryIndustry);
+    var queryLocation = parameterSearch[0].split('=').pop();
+    var queryLevel = parameterSearch[1].split('=').pop();
+    var queryIndustry =parameterSearch[2].split('=').pop();
+    console.log(queryLocation)
+    searchApi(queryLocation, queryLevel, queryIndustry);
   }
 
-function lunchSearch(inputLoc, levelJo, industryJo) {
-    var requestUrl = `https://www.themuse.com/api/public/jobs?location=${inputLoc}&level=${levelJo}&industry=${industryJo}&page=${1}`;
+
+  function searchApi(queryLocation, queryLevel, queryIndustry) {
+    var requestUrl = `https://www.themuse.com/api/public/jobs?location=${queryLocation}&level=${queryLevel}&industry=${queryIndustry}&page=${1}`;
   
     fetch(requestUrl)
       .then(function (response) {
@@ -21,21 +23,26 @@ function lunchSearch(inputLoc, levelJo, industryJo) {
       })
   }
 
-  function mapLocation(inputLoc) {
+
+  getParams();
+
+
+
+//   function mapLocation(inputLoc) {
   
-    var requestUrl1 = "https://api.openweathermap.org/data/2.5/weather?q=" + inputLoc +
-      "&appid=5ec79846ae2fb1a4571dea79f4797492"
+//     var requestUrl1 = "https://api.openweathermap.org/data/2.5/weather?q=" + inputLoc +
+//       "&appid=5ec79846ae2fb1a4571dea79f4797492"
   
-    fetch(requestUrl1)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data1) {
-        console.log(data1)
-        latitude = data1.coord.lat
-        longitude = data1.coord.lon
-        console.log(latitude)
-        console.log(longitude)
-      })
-  }
+//     fetch(requestUrl1)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data1) {
+//         console.log(data1)
+//         latitude = data1.coord.lat
+//         longitude = data1.coord.lon
+//         console.log(latitude)
+//         console.log(longitude)
+//       })
+//   }
   
