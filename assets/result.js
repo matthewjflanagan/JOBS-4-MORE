7de34a020abdaa8cc9e6da320b2d50b264ec5435
yanghuixi1 <yanghui.xi1@gmail.com>
2021-10-11 15:44:23 -0400

    Get Jooble API working

--- assets/result.js
+++ assets/result.js
@@ -1,111 +1,107 @@
+var joobleAPIKey = "2602e22f-9b58-49a6-bbdf-15679eaaf126";
+var themuseAPIKey;
+var jobsContainer = $("#results");
+
 function getParams() {
-    // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-    var parameterSearch = document.location.search.split('&');
-  console.log(parameterSearch)
-    // Get the query and format values
-    var queryLocation = parameterSearch[0].split('=').pop();
-    var queryLevel = parameterSearch[1].split('=').pop();
-    var queryIndustry =parameterSearch[2].split('=').pop();
-    console.log(queryLocation)
-    searchApi(queryLocation, queryLevel, queryIndustry);
-  }
+  var parameterSearch = document.location.search.split("&");
+  console.log(parameterSearch);
+  // Get the query and format values
+  var queryLocation = parameterSearch[0].split("=").pop();
+  var queryLevel = parameterSearch[1].split("=").pop();
+  var queryCategory = parameterSearch[2].split("=").pop();
+  console.log(queryLocation);
+  return {
+    location: queryLocation,
+    level: queryLevel,
+    category: queryCategory,
+  };
+}
 
+function writeJob(job) {
+  // Job is an object that has these properties:
+  // {
+  //    title: string,
+  //    company: string,
+  //    location: string,
+  //    description: string,
+  //    postedDate: datetime
+  //    level: string (optional)
+  // }
+  var jobCardEl = $("<div>");
+  var descriptionEl = $("<div>");
+  descriptionEl.html(job.description);
+  var jobNameEl = $("<h3>");
+  jobNameEl.text(job.title);
+  jobCardEl.append(jobNameEl);
+  var companyEl = $("<p>");
+  companyEl.text("Company: " + job.company);
+  jobCardEl.append(companyEl);
+  var locationEl = $("<p>");
+  locationEl.text("Location: " + job.location);
+  jobCardEl.append(locationEl);
+  var updatedEl = $("<p>");
+  updatedEl.text("Job Updated Date: " + job.postedDate);
+  jobCardEl.append(updatedEl);
+  jobCardEl.append(descriptionEl);
+  jobsContainer.append(jobCardEl);
+}
 
-  function searchApi(queryLocation, queryLevel, queryIndustry) {
-    var requestUrl = `https://www.themuse.com/api/public/jobs?location=${queryLocation}&level=${queryLevel}&industry=${queryIndustry}&page=${1}`;
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-        for (var i = 0; i < data.results.length; i++){
+function searchJooble(queryLocation, queryLevel, queryCategory) {
+  var searchParams = {
+    location: decodeURIComponent(queryLocation),
+    keywords:
+      decodeURIComponent(queryCategory) + " " + decodeURIComponent(queryLevel),
+    page: 1,
+  };
 
-          var jobName = data.results[i].name
-          console.log(jobName)
-          var jobCompany = data.results[i].company.name
-          console.log(jobCompany)
-          var jobLocation = data.results[i].locations[0].name;
-          console.log(jobLocation)
-          var jobPublicationDate = data.results[i].publication_date;
-          console.log(jobPublicationDate)
-          var jobLevel = data.results[i].levels[0].name
-          console.log(jobLevel)
-          var jobDescription = data.results[i].contents;
-          console.log (jobDescription)
-          var  jobsContainer = $("#results")
-          var jobCard = $("<div>");
-          var descriptionElem = $("<div>")
-          descriptionElem.html(jobDescription);
-          var jobNames = $("<h3>")
-          jobNames.text(jobName)
-          jobCard.append(jobNames)
-          //   Another way of appending the company name:   jobCard.append(`<h3>${jobName}</h3>`);
-          var companies = $("<p>")
-          companies.text("Company:" + jobCompany)
-          jobCard.append(companies)
-          // Another way of appending the company:  jobCard.append(`<p>Company: ${jobCompany}</p>`);
-          var locations = $("<p>")
-          locations.text("Location:" + jobLocation)
-          jobCard.append(locations)
-        // Another way of appending the location:  jobCard.append(`<p>Location: ${jobLocation}</p>`);
-        var publications = $("<p>")
-          publications.text("Job Posted Date:" + jobPublicationDate)
-          jobCard.append(publications)
+  var requestUrl = `https://jooble.org/api/${joobleAPIKey}`;
+  fetch(requestUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(searchParams),
+  })
+    .then((data) => data.arrayBuffer())
+    .then((buffer) => String.fromCharCode.apply(null, new Int8Array(buffer)))
+    .then((result) => JSON.parse(result))
+    .then(function (data) {
+      console.log(data);
+      for (var i = 0; i < data.jobs.length; i++) {
+        var job = {
+          title: data.jobs[i].title,
+          company: data.jobs[i].company,
+          location: data.jobs[i].location,
+          description: data.jobs[i].snippet,
+          postedDate: data.jobs[i].updated,
+          level: null,
+        };
+        writeJob(job);
+      }
+    });
+}
 
-        // Another way of appending the publication:  jobCard.append(`<p>Job Posted Date: ${jobPublicationDate}</p>`);
-        jobCard.append(descriptionElem);
-        jobsContainer.append(jobCard);
+function searchMuse(queryLocation, queryLevel, queryCategory) {
+  var requestUrl = `https://www.themuse.com/api/public/jobs?location=${queryLocation}&level=${queryLevel}&category=${queryCategory}&page=${1}`;
 
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      for (var i = 0; i < data.results.length; i++) {
+        var job = {
+          title: data.results[i].name,
+          company: data.results[i].company.name,
+          location: data.results[i].locations[0].name,
+          description: data.results[i].contents,
+          postedDate: data.results[i].publication_date,
+          level: data.results[i].levels[0].name,
+        };
+        writeJob(job);
+      }
+    });
+}
 
-
-        }
-
-
-      })
-  }
-
-
-  getParams();
-
-
-
-//   function mapLocation(inputLoc) {
-  
-//     var requestUrl1 = "https://api.openweathermap.org/data/2.5/weather?q=" + inputLoc +
-//       "&appid=5ec79846ae2fb1a4571dea79f4797492"
-  
-//     fetch(requestUrl1)
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (data1) {
-//         console.log(data1)
-//         latitude = data1.coord.lat
-//         longitude = data1.coord.lon
-//         console.log(latitude)
-//         console.log(longitude)
-//       })
-// //   }
-// var issueEl = document.createElement('a');
-// issueEl.classList = 'list-item flex-row justify-space-between align-center';
-// issueEl.setAttribute('href', issues[i].html_url);
-// issueEl.setAttribute('target', '_blank');
-// var displayWarning = function (repo) {
-//   limitWarningEl.textContent = 'To see more than 30 issues, visit ';
-
-//   var linkEl = document.createElement('a');
-//   linkEl.textContent = 'GitHub.com';
-//   linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
-//   linkEl.setAttribute('target', '_blank');
-
-//   // This will appear on the bottom of the page.
-//   limitWarningEl.appendChild(linkEl);
-// };
-// var userName = document.createElement('h3');
-// var issueTitle = document.createElement('p');
-// userName.textContent = data[i].user.login;
-// issueTitle.textContent = data[i].title;
-// issueContainer.append(userName);
-// issueContainer.append(issueTitle);
+var params = getParams();
+//searchMuse(params.location, params.level, params.category);
+searchJooble(params.location, params.level, params.category);
